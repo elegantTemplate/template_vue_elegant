3fb4677f31a0624aba73b9bda15495339da0c8ea
longer567 <1652423708@qq.com>
2020-04-01 10:37:03 +0800

    feat: delete engine.js

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 import Home from './pages/home/home.vue'
 import Show from './pages/show/show.vue'
 
-const routerPath = LG_CONFIG_ROUTERPATH
+const routerPath = 'LG_CONFIG_ROUTERPATH'
 Vue.use(VueRouter)
 const routes = [{
         path: `${routerPath}/home`,
